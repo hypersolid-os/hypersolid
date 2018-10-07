384ffae7e3c15f234bdc5f24e637dd3cf606af1f
Andi Dittrich <andi@andidittrich.de>
2018-10-07 17:42:51 +0200

    networking

--- build.sh
+++ build.sh
@@ -11,6 +11,5 @@ docker container rm raspberry-env
 # run postbuild
 docker run --tty --name raspberry-env --interactive --privileged raspberry-build /home/build/rootfs/.setup/postinstall.sh
 
-
 # copy files
 docker cp raspberry-env:/home/build/ tmp/
