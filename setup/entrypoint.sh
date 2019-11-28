ae0335d4ca6cb8653fa6fc23a9d901ea50620cdf
Andi Dittrich <andi@andidittrich.de>
2019-11-28 10:00:29 +0100

    enhanced logging

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe
+set -x
 
 export BUILDFS=/opt/build
 export ROOTFS=/opt/rootfs
@@ -13,10 +13,13 @@ if [ -f "/.buildready" ]; then
     exit 0
 fi
 
-echo "first container startup - running build"
+# get function utilities
+source $ROOTFS/.build/functions
 
 # load config
 source $ROOTFS/.build/config
+
+log_info "initial container startup - running build"
 
 # create firstrun flag
 touch /.buildready
@@ -29,26 +32,43 @@ cp $ROOTFS/etc/apt/trusted.gpg.d/* $BUILDFS/etc/apt/trusted.gpg.d
 multistrap \
     --arch $CONF_ARCH \
     --dir $BUILDFS \
-    --file /etc/multistrap/multistrap.ini
+    --file /etc/multistrap/multistrap.ini || {
+        panic "multistrap failed"
+    }
 
 # copy additional files
 cp -R $ROOTFS/. $BUILDFS
 
 # busybox libmusl - just override binary
+# required to enabled full busybox support within initramfs (e.g. DNS)
+BUSYBOX_BIN=
 case "$CONF_ARCH" in
-        armel)
-            echo "downloading busybox busybox-armv5l"
-            wget -O $BUILDFS/bin/busybox https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-armv5l
-            ;;
-        *)
-            # x86 64 default
-            echo "downloading busybox-x86_64"
-            wget -O $BUILDFS/bin/busybox https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64
+    armel)
+        BUSYBOX_BIN="busybox-armv5l"
+        ;;
+    armhf)
+        BUSYBOX_BIN="busybox-armv7l"
+        ;;
+    arm64)
+        BUSYBOX_BIN="busybox-armv8l"
+        ;;
+    *)
+        # x86 64 default
+        BUSYBOX_BIN="busybox-x86_64"
 esac
+
+log_info "downloading ${BUSYBOX_BIN}" 
+
+# trigger download
+wget -O $BUILDFS/bin/busybox https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/${BUSYBOX_BIN} && {
+    log_success "done"
+} || {
+    panic "download failed"
+}
 
 # run post multistrap script hook ?
 if [ -x "$BUILDFS/.build/scripts/post-multistrap.sh" ]; then
-    echo "hook [post-multistrap]"
+    log_info "hook [post-multistrap]"
     $BUILDFS/.build/scripts/post-multistrap.sh
 fi
 
@@ -65,12 +85,12 @@ if [ "$CONF_ARCH" == "armel" ]; then
 fi
 
 # chroot into fs (emulated mode)
-echo "chroot into rootfs to execute postinstall actions"
+log_info "chroot into rootfs to execute postinstall actions"
 chroot $BUILDFS /bin/bash -c "/.build/postinstall-chroot.sh"
 
 # run post configure script hook ?
 if [ -x "$BUILDFS/.build/scripts/post-configure.sh" ]; then
-    echo "hook [post-configure]"
+    log_info "hook [post-configure]"
     $BUILDFS/.build/scripts/post-configure.sh
 fi
 
@@ -91,10 +111,15 @@ mksquashfs $BUILDFS $BOOTFS/system.img \
         $BUILDFS/etc/initramfs-tools \
         $BUILDFS/usr/bin/qemu-* \
         $BUILDFS/vmlinuz* \
-        $BUILDFS/initrd*
+        $BUILDFS/initrd* \
+&& {
+    log_success "squashfs file created in $BUILDFS $BOOTFS/system.img"
+} || {
+    panic "failed to create squashfs image"
+}
 
 # run post build script hook ?
 if [ -x "$BUILDFS/.build/scripts/post-build.sh" ]; then
-    echo "hook [post-build]"
+    log_info "hook [post-build]"
     $BUILDFS/.build/scripts/post-build.sh
 fi
